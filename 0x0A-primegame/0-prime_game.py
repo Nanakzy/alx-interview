de6e938b99d4a949e3be5d960e1a1a1d90a525eb
Nanakzy <nanakzy@gmail.com>
2024-09-03 12:46:42 -0700

    commit changes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,21 +1,8 @@
 #!/usr/bin/python3
-"""Prime Game Module
-
-This module defines functions for simulating the Prime Game, where two
-players, Maria and Ben, take turns removing primes and their multiples
-from a list of numbers. The winner is determined by the player who forces
-the other player to have no valid moves.
-"""
-
+"""Prime game"""
 
 def is_prime(num):
-    """Checks if a number is prime.
-    Args:
-        num (int): The number to check.
-
-    Returns:
-        bool: True if the number is prime, False otherwise.
-    """
+    """Checks if a number is prime."""
     if num <= 1:
         return False
     if num <= 3:
@@ -29,30 +16,12 @@ def is_prime(num):
         i += 6
     return True
 
-
 def remove_prime_and_multiples(nums, prime):
-    """Removes a prime and all its multiples from a list.
-
-    Args:
-        nums (list): The list of numbers.
-        prime (int): The prime number to remove along with its multiples.
-
-    Modifies:
-        nums (list): The list is modified in place, removing the prime
-                     and its multiples.
-    """
+    """Removes a prime and its multiples from the list."""
     nums[:] = [n for n in nums if n % prime != 0]
 
-
 def play_round(n):
-    """Simulates one round of the Prime Game.
-
-    Args:
-        n (int): The maximum number in the list to play with.
-
-    Returns:
-        bool: True if Maria wins the round, False if Ben wins.
-    """
+    """Plays one round of the game."""
     nums = list(range(1, n + 1))
     maria_turn = True
 
@@ -69,19 +38,8 @@ def play_round(n):
 
         maria_turn = not maria_turn
 
-
 def isWinner(x, nums):
-    """Determines the overall winner of the Prime Game after x rounds.
-
-    Args:
-        x (int): The number of rounds to be played.
-        nums (list): A list of integers representing the maximum number
-                     in the list for each round.
-
-    Returns:
-        str: "Maria" if Maria wins more rounds, "Ben" if Ben wins more
-             rounds, or None if it's a tie.
-    """
+    """Determines the overall winner of the Prime Game."""
     maria_wins, ben_wins = 0, 0
 
     for n in nums:
